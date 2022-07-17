2460b3e4aa85554ba411dc8c43bf62a7ea03dc8b
Jason Cabral <jasoncabral329@gmail.com>
2022-07-17 23:19:31 +0800

    update in user navigation

--- src/container/users/Navbar.jsx
+++ src/container/users/Navbar.jsx
@@ -3,15 +3,24 @@ import NavbarCategories from "../../component/NavbarCategory";
 import NavbarSearch from "../../component/NavbarSearch";
 import { AiOutlineMenu } from "react-icons/ai";
 import { useHistory } from "react-router-dom";
-import {useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import LoginUserNavbar from "../../component/LoginUserNavbar";
+import Dropdown from "../../component/Dropdown";
+import Avatar from "react-avatar";
+import {
+  AiOutlineBell,
+  AiOutlineLogout,
+  AiOutlineMessage,
+} from "react-icons/ai";
+import { signout } from "../../redux/actions";
 const Navbar = ({ categories, ...props }) => {
   const [mobile, setMobile] = useState(false);
-  const {user} = useSelector(state => state.auth);
+  const { user } = useSelector((state) => state.auth);
   const history = useHistory();
+  const dispatch = useDispatch();
   const RegisterSignUp = () => {
     return (
-      <>  
+      <>
         <div
           className="btn-login"
           onClick={(e) => {
@@ -20,10 +29,10 @@ const Navbar = ({ categories, ...props }) => {
         >
           Login
         </div>
-        </>
+      </>
     );
   };
- 
+
   return (
     <div className="navbar-container ">
       <div className="navbar-content">
@@ -33,17 +42,47 @@ const Navbar = ({ categories, ...props }) => {
             <NavbarSearch />
           </div>
           <div className="sign-up-sign-in-container">
-            {
-              user ? <LoginUserNavbar /> : <RegisterSignUp />
-            }
-            
+            {user ? <LoginUserNavbar /> : <RegisterSignUp />}
           </div>
+          <div className="pointer user-navbar-mobile">
+            {user ? (
+              <Dropdown
+                onPoint={
+                  user ? (
+                    user.profile.url ? (
+                      <Avatar src={user.profile.url} size={35} round={true} />
+                    ) : (
+                      <Avatar name={user.name} size={35} round={true} />
+                    )
+                  ) : null
+                }
+              >
+                <div className="p-2 w-100 d-flex justify-content-start align-items-center pointer menu-user-nav">
+                    <AiOutlineBell className="pointer" size={23} />
+                    <div className="ps-2">Notifications </div>
+                </div>
+                <div className="p-2 w-100 d-flex justify-content-start align-items-center pointer menu-user-nav">
+                    <AiOutlineMessage className="pointer" size={23} />
+                    <div className="ps-2"> Messages </div>
+                </div>
+                <div className="p-2 w-100 d-flex justify-content-start align-items-center pointer menu-user-nav" 
+                onClick={() => {
+                  history.push("/");
+                  dispatch(signout());
+                }}
+                >
+                    <AiOutlineLogout className="pointer" size={23} />
+                    <div className="ps-2">Logout </div>
+                </div>
+              </Dropdown>
+            ) : null}
+          </div>
+
           <AiOutlineMenu
-            size={24}
+            size={35}
             className="pointer menu-navbar"
             onClick={(e) => {
               setMobile(!mobile);
-            
             }}
           />
         </div>
@@ -57,7 +96,7 @@ const Navbar = ({ categories, ...props }) => {
       </div>
       <div
         className={`mobile-navbar-container ${
-          mobile ? "slide-in-right" : "slide-out-right"
+          mobile ? "scale-in-right" : "slide-out-right"
         }`}
       >
         <NavbarCategories
